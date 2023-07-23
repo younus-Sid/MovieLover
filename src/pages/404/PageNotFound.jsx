0df975d0479c78b9a67610ebb928a1cd1c678cb2
Younus Siddique <karocode11@gmail.com>
2023-07-23 23:37:57 +0530

    Added files

---
+++ src/pages/404/PageNotFound.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+import ContentWrapper from "../../components/contentWrapper/ContentWrapper";
+import "./style.scss";
+
+const PageNotFound = () => {
+  return (
+    <div className="pageNotFound">
+      <ContentWrapper>
+        <span className="bigText">404</span>
+        <span className="smallText">Page not found!</span>
+      </ContentWrapper>
+    </div>
+  );
+};
+
+export default PageNotFound;
